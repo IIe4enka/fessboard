68abb44a408c2637a0df3cc06fd7195a66bb4568
MtankoAwara <mikhail.tanko@awara-it.com>
2023-01-15 00:01:32 +0300

    Commented all models

--- homepage/models.py
+++ homepage/models.py
@@ -65,7 +65,7 @@ class FieldSphere(models.Model):
 
 
 
-
+# Эта модель не нужна ???. В теории она должна была описывать Группы студентов в проектах, на основании её строилась бы GroupsInProjects
 class Groups(models.Model):
     curator_student = models.ForeignKey('Students', on_delete=models.SET_NULL, null=True, blank=True,)
 
@@ -73,6 +73,7 @@ class Groups(models.Model):
         managed = False
         db_table = 'groups'
 
+# Эта модель не нужна ???. В теории она должна была быть ManyToMany связью между Group и Project таблицами
 class GroupsInProjects(models.Model):
     project = models.ForeignKey('Projects', on_delete=models.SET_NULL, null=True, blank=True,)
     group = models.ForeignKey(Groups, on_delete=models.SET_NULL, null=True, blank=True,)
@@ -81,6 +82,8 @@ class GroupsInProjects(models.Model):
         managed = False
         db_table = 'groups_in_projects'
 
+
+# Эта таблица отражает ManyToMany связь между Проектными менеджерами и Мероприятиями. Здесь указаны все Менеджеры во всех мероприятиях.
 class ManagersInEvent(models.Model):
     event = models.ForeignKey(Event, on_delete=models.SET_NULL, null=True, blank=True,)
     student = models.ForeignKey('Students', on_delete=models.SET_NULL, null=True, blank=True,)
@@ -89,6 +92,8 @@ class ManagersInEvent(models.Model):
         managed = False
         db_table = 'managers_in_Event'
 
+
+# Эта таблица отражает ManyToMany связь между Проектными менеджерами и Проектами. Здесь указаны все Менеджеры во всех проектах
 class ManagersInProjects(models.Model):
     project = models.ForeignKey('Projects', on_delete=models.SET_NULL, null=True, blank=True,)
     student = models.ForeignKey('Students', on_delete=models.SET_NULL, null=True, blank=True,)
@@ -97,6 +102,8 @@ class ManagersInProjects(models.Model):
         managed = False
         db_table = 'managers_in_projects'
 
+
+# Эта таблица отражает ManyToMany связь между Участниками (студентами) и Мероприятиями. Здесь указаны все Участники во всех Мероприятиях
 class ParticipantsInEvent(models.Model):
     event = models.ForeignKey(Event, on_delete=models.SET_NULL, null=True, blank=True,)
     student = models.ForeignKey('Students', on_delete=models.SET_NULL, null=True, blank=True,)
@@ -105,6 +112,8 @@ class ParticipantsInEvent(models.Model):
         managed = False
         db_table = 'participants_in_Event'
 
+
+# Эта таблица хранит в себе список Типов проектов, а также информацию о том к какой Сфере относится Тип проекта. Это нужна для сбора статистики по направлениям проектов
 class ProjectFields(models.Model):
     field_id = models.AutoField(primary_key=True)
     field = models.CharField(max_length=255)
@@ -115,6 +124,7 @@ class ProjectFields(models.Model):
         db_table = 'project_fields'
 
 
+# Эта модель отражает список Грейдов (уровней сложности) проектов
 class ProjectGrades(models.Model):
     grade_id = models.AutoField(primary_key=True)
     grade = models.CharField(max_length=255)
@@ -123,6 +133,8 @@ class ProjectGrades(models.Model):
         managed = False
         db_table = 'project_grades'
 
+
+# Эта модель хранит в себе записи о всех проектах
 class Projects(models.Model):
     project_id = models.AutoField(primary_key=True)
     project_name = models.CharField(max_length=255)
@@ -143,6 +155,8 @@ class Projects(models.Model):
         managed = False
         db_table = 'projects'
 
+
+# Эта модель хранит в себе записи о существующих географических регионах для Университетов
 class Regions(models.Model):
     region_id = models.AutoField(primary_key=True)
     region = models.CharField(max_length=255)
@@ -152,6 +166,8 @@ class Regions(models.Model):
         managed = False
         db_table = 'regions'
 
+
+# Эта модель хранит в себе список всех статусов студентов (например: Учится, Отчислен, В академе)
 class StudentStatuses(models.Model):
     student_status_id = models.AutoField(primary_key=True)
     student_status = models.CharField(max_length=255)
@@ -160,6 +176,8 @@ class StudentStatuses(models.Model):
         managed = False
         db_table = 'student_statuses'
 
+
+# Эта модель хранит список всех студентов на факультете
 class Students(models.Model):
     student_id = models.AutoField(primary_key=True)
     student_surname = models.CharField(max_length=255)
@@ -178,6 +196,8 @@ class Students(models.Model):
         db_table = 'students'
 
 
+
+# Эта модель не нужна ??? Она в теории должна была отражать ManyToMany связь между Student и Group, учитывать всех студентов во всех группах
 class StudentsInGroups(models.Model):
     group = models.ForeignKey(Groups, on_delete=models.SET_NULL, null=True, blank=True,)
     student = models.ForeignKey(Students, on_delete=models.SET_NULL, null=True, blank=True,)
@@ -186,6 +206,8 @@ class StudentsInGroups(models.Model):
         managed = False
         db_table = 'students_in_groups'
 
+
+# Эта модель хранит список всех преподавателей на факультете
 class Teachers(models.Model):
     teacher_id = models.AutoField(primary_key=True)
     teacher_surname = models.CharField(max_length=255)
@@ -198,6 +220,7 @@ class Teachers(models.Model):
         db_table = 'teachers'
 
 
+# Преподаватели в мероприятиях. ManyToMany
 class TeachersInEvent(models.Model):
     teacher = models.ForeignKey(Teachers, on_delete=models.SET_NULL, null=True, blank=True,)
     event = models.ForeignKey(Event, on_delete=models.SET_NULL, null=True, blank=True,)
@@ -206,6 +229,8 @@ class TeachersInEvent(models.Model):
         managed = False
         db_table = 'teachers_in_Event'
 
+
+# Преподаватели в проектах. ManyToMany
 class TeachersInProjects(models.Model):
     teacher = models.ForeignKey(Teachers, on_delete=models.SET_NULL, null=True, blank=True,)
     project = models.ForeignKey(Projects, on_delete=models.SET_NULL, null=True, blank=True,)
@@ -214,6 +239,8 @@ class TeachersInProjects(models.Model):
         managed = False
         db_table = 'teachers_in_projects'
 
+
+# Эта модель хранит список всех Партнёрских университетов
 class Universities(models.Model):
     university_id = models.AutoField(primary_key=True)
     university_name = models.CharField(max_length=255)
