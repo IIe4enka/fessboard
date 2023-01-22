dcc66f1be2e67e91b74ccc9eef6a9557c1317008
Negroslav <108018442+Negroslav@users.noreply.github.com>
2023-01-22 11:36:44 +0300

    Update models.py

--- students/models.py
+++ students/models.py
@@ -20,9 +20,9 @@ class Region(models.Model):
 # Эта модель хранит список всех Партнёрских университетов
 class University(models.Model):
 
-    name    = models.CharField(max_length=255, verbose_name="???")
+    name    = models.CharField(max_length=255, verbose_name="Название университета-партнёра")
     logo    = models.TextField(verbose_name="???")
-    region  = models.ForeignKey(Region, on_delete=models.SET_NULL, null=True, blank=True, verbose_name="???")
+    region  = models.ForeignKey(Region, on_delete=models.SET_NULL, null=True, blank=True, verbose_name="Территориальное расположение университета-партнёра")
 
     status  = models.CharField(max_length=15, verbose_name="Статус", choices=(
         (1, 'bachelor'),
@@ -37,12 +37,12 @@ class Profile(models.Model):
 
     user                    = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True, verbose_name="Пользователь", help_text="Каждый пользователь имеет свой profile")
 
-    surname                 = models.CharField(max_length=255, verbose_name="???")
-    name                    = models.CharField(max_length=255, verbose_name="???")
-    midname                 = models.CharField(max_length=255, verbose_name="???")
-    bachelors_start_year    = models.TextField(blank=True, null=True, verbose_name="???")
-    masters_start_year      = models.TextField(blank=True, null=True, verbose_name="???")
-    university              = models.ForeignKey(University, on_delete=models.SET_NULL, null=True, blank=True, verbose_name="???")
+    surname                 = models.CharField(max_length=255, verbose_name="Фамилия студента")
+    name                    = models.CharField(max_length=255, verbose_name="Имя студента")
+    midname                 = models.CharField(max_length=255, verbose_name="Отчество студента")
+    bachelors_start_year    = models.TextField(blank=True, null=True, verbose_name="Дата начала обучения на бакалавра")
+    masters_start_year      = models.TextField(blank=True, null=True, verbose_name="Дата начала обучения на магистра")
+    university              = models.ForeignKey(University, on_delete=models.SET_NULL, null=True, blank=True, verbose_name="Университет, в котором обучается студент")
 
     projects                = models.ManyToManyField(Project, verbose_name="Проекты")
     groups                  = models.ManyToManyField(Event, verbose_name="События")
